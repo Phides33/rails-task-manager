0608199c22bf99ea27bc15cf98db6706e9b3a3da
Philippe Despreaux <despreaux.philippe@wanadoo.fr>
2017-11-07 11:29:04 +0100

    model generated

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171107102143) do
+ActiveRecord::Schema.define(version: 20171107102801) do
 
-  create_table "restaurants", force: :cascade do |t|
+  create_table "tasks", force: :cascade do |t|
     t.string   "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
